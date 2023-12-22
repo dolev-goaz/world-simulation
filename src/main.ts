47129ec55cac21455d4bfa48728fe9683ce46237
dolev goaz <www.dolev@gmail.com>
2023-12-22 22:13:40 +0200

    basic simulation loop

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import config from "./config.json";
 const simulation = new Simulation();
 
 
-for (let i = 0; i < config.StepCount; i++) {
+for (let i = 0; i < config.StepCount; ++i) {
     simulation.step();
+    sleep(500);
+}
+
+function sleep(time: number) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, time);
+    });
 }
--- src/simulation/simulation.ts
+++ src/simulation/simulation.ts
@@ -1,5 +1,6 @@
 import { Map } from "./map";
 import config from "../config.json";
+import { Cell } from "./cell";
 
 export class Simulation {
     map: Map;
@@ -10,5 +11,26 @@ export class Simulation {
 
     step() {
         this.map.draw();
+        this.map.cells.forEach(this.updateCell.bind(this));
+    }
+
+    private updateCell(cell: Cell) {
+        const neighbors = this.getNeighbors(cell);
+    }
+
+    private getNeighbors(cell: Cell) {
+        return [
+            this.getNeighbor(cell, 0, 1),
+            this.getNeighbor(cell, 0, -1),
+            this.getNeighbor(cell, 1, 0),
+            this.getNeighbor(cell, -1, 0),
+        ].filter(Boolean);
+    }
+
+    private getNeighbor(cell: Cell, dy: number, dx: number) {
+        const xIndex = (cell.indexX + dx) % config.CellsInRow;
+        const yIndex = (cell.indexY + dy) % config.CellsInColumn;
+
+        return this.map.cells[yIndex * config.CellsInRow + xIndex];
     }
 }
