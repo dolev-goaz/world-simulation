eb3b3d4a76aeab3a1f32f0810603c4dd86c9b954
dolev goaz <www.dolev@gmail.com>
2023-12-24 22:22:58 +0200

    controls

--- src/main.ts
+++ src/main.ts
@@ -18,19 +18,74 @@ function step() {
     simulation.moveNextGen();
     simulation.map.draw();
 }
-const button = document.createElement("button");
-button.innerText = "Step"
-button.onclick = step;
-document.body.appendChild(button)
 
-for (let i = 0; i < config.StepCount - 1; ++i) {
-    step();
-    await sleep(400)
+
+setupControls();
+simulation.map.draw();
+
+function createLoopControls() {
+    const initialSliderValue = 2;
+    let timePerFrame = 1000 / initialSliderValue;
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.title = 'auto step';
+    let loopInterval: NodeJS.Timeout;
+    checkbox.oninput = () => {
+        if (!checkbox.checked) return;
+
+        step();
+        loopInterval = setInterval(() => {
+            if (!checkbox.checked) {
+                clearInterval(loopInterval);
+                return;
+            }
+            step();
+        }, timePerFrame);
+    }
+
+    const slider = document.createElement('input');
+    slider.type = 'range';
+    slider.min = '1';
+    slider.max = '5'
+    slider.step = '1';
+    slider.value = initialSliderValue.toString();
+    slider.title = `steps per second- ${slider.value}`;
+
+    slider.oninput = () => {
+        timePerFrame = 1000 / Number(slider.value);
+        slider.title = `steps per second- ${slider.value}`;
+        clearInterval(loopInterval);
+        loopInterval = setInterval(() => {
+            if (!checkbox.checked) {
+                clearInterval(loopInterval);
+                return;
+            }
+            step();
+        }, timePerFrame);
+
+    }
+    return { slider, checkbox }
 }
-step();
 
-function sleep(time: number) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, time);
-    });
+function createStepButton() {
+    const button = document.createElement("button");
+    button.innerText = "Step"
+    button.onclick = step;
+    return button;
+}
+
+function setupControls() {
+    const stepButton = createStepButton();
+    const loopControls = createLoopControls();
+
+    const loopContainer = document.createElement('div');
+    loopContainer.appendChild(loopControls.checkbox);
+    loopContainer.appendChild(loopControls.slider);
+
+    const controlsContainer = document.createElement('div');
+    controlsContainer.classList.add('controls')
+    controlsContainer.appendChild(stepButton);
+    controlsContainer.appendChild(loopContainer);
+    document.body.appendChild(controlsContainer);
 }
