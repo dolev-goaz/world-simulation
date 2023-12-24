0a028ca0fc5f252f8f8b51eaec279ab7dc2d9c88
dolev goaz <www.dolev@gmail.com>
2023-12-24 21:03:18 +0200

    cell hover info

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,13 @@ const areaMap = config.Map1.split('\n').map((line) => line.split('')).flat() as 
 const simulation = new Simulation(areaMap);
 
 function step() {
+    // // use this to show next step info
+    // simulation.calcNextGen();
+    // simulation.map.draw();
+    // simulation.moveNextGen();
     simulation.calcNextGen();
-    simulation.map.draw();
     simulation.moveNextGen();
+    simulation.map.draw();
 }
 const button = document.createElement("button");
 button.innerText = "Step"
@@ -17,9 +21,9 @@ document.body.appendChild(button)
 
 for (let i = 0; i < config.StepCount - 1; ++i) {
     step();
-    await sleep(800)
+    await sleep(400)
 }
-button.click();
+step();
 
 function sleep(time: number) {
     return new Promise((resolve) => {
--- src/simulation/worldMap.ts
+++ src/simulation/worldMap.ts
@@ -6,13 +6,12 @@ export class WorldMap {
 
     private canvas: HTMLCanvasElement;
     private ctx: CanvasRenderingContext2D;
+    private cellTooltip: HTMLElement;
 
     constructor(cells: Cell[] = []) {
-        this.canvas = document.createElement("canvas");
-        this.canvas.width = config.CellSize * config.CellsInColumn;
-        this.canvas.height = config.CellSize * config.CellsInRow;
-
-        document.body.appendChild(this.canvas);
+        const htmlElements = this.initializeMapHTML();
+        this.canvas = htmlElements.canvas;
+        this.cellTooltip = htmlElements.cellTooltip;
 
         const ctx = this.canvas.getContext("2d");
         if (!ctx) throw new Error("An error has occured. please refresh");
@@ -28,10 +27,50 @@ export class WorldMap {
     draw() {
         this.clear();
         this.cells.forEach(this.drawCell.bind(this));
+        if (!this.cellTooltip.hidden) this.onCanvasHoverEnd() // info isnt relevant
     }
 
     clear() {
         this.ctx.fillStyle = 'white';
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    private onCanvasHover(event: MouseEvent) {
+        const x = Math.floor(event.offsetX / config.CellSize);
+        const y = Math.floor(event.offsetY / config.CellSize);
+
+        const cell = this.cells[y * config.CellsInRow + x];
+        
+        this.cellTooltip.innerText = JSON.stringify(cell, null, '\t');
+        this.cellTooltip.hidden = false;
+    }
+    private onCanvasHoverEnd() {
+        this.cellTooltip.hidden = true;
+        this.cellTooltip.innerText = '';
+    }
+
+    private initializeMapHTML() {
+        const canvasParent = document.createElement('div');
+
+        const canvas = document.createElement("canvas");
+        canvas.width = config.CellSize * config.CellsInColumn;
+        canvas.height = config.CellSize * config.CellsInRow;
+        canvas.onmousemove = this.onCanvasHover.bind(this);
+        canvas.onmouseleave = this.onCanvasHoverEnd.bind(this);
+        canvasParent.appendChild(canvas);
+
+        const cellTooltip = document.createElement("p");
+        cellTooltip.classList.add('tooltip');
+        canvasParent.appendChild(cellTooltip);
+
+        canvasParent.classList.add('canvas-container');
+
+        document.body.appendChild(canvasParent);
+
+
+        return {
+            canvas,
+            cellTooltip
+        };
     }
 }
