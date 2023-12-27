e2630ad5733ec02724f373531112d22eb83a50f5
dolev-goaz <www.dolev@gmail.com>
2023-12-27 15:15:34 +0200

    basic excel export

--- src/main.ts
+++ src/main.ts
@@ -1,91 +1,106 @@
 import { Simulation } from "./simulation/simulation";
-import config from "./config.json"
+import * as ExcelJS from "exceljs";
+import * as FileSaver from "file-saver";
+import config from "./config.json";
 import { AreaShortReversed, TArea } from "./simulation/area";
 
+async function writeSpreadSheet() {
+  const workbook = new ExcelJS.Workbook();
+  const worksheet = workbook.addWorksheet("test");
+  worksheet.columns = [{ header: "Test Header", key: "test" }];
+  worksheet.addRow({ test: 5 });
+
+  const buffer = await workbook.xlsx.writeBuffer();
+  FileSaver.saveAs(new Blob([buffer]), "simulation_export.xlsx");
+}
+
+writeSpreadSheet();
+
 const areaMap = config.Maps[0]
-    .split('\n')
-    .map((line) => line.split(''))
-    .flat()
-    .map((areaShort) => AreaShortReversed[areaShort as keyof typeof AreaShortReversed]) as TArea[];
+  .split("\n")
+  .map((line) => line.split(""))
+  .flat()
+  .map(
+    (areaShort) =>
+      AreaShortReversed[areaShort as keyof typeof AreaShortReversed]
+  ) as TArea[];
 const simulation = new Simulation(areaMap);
 
 function step() {
-    // // use this to show next step info
-    // simulation.calcNextGen();
-    // simulation.map.draw();
-    // simulation.moveNextGen();
-    simulation.calcNextGen();
-    simulation.moveNextGen();
-    simulation.map.draw();
+  // // use this to show next step info
+  // simulation.calcNextGen();
+  // simulation.map.draw();
+  // simulation.moveNextGen();
+  simulation.calcNextGen();
+  simulation.moveNextGen();
+  simulation.map.draw();
 }
-
 
 setupControls();
 simulation.map.draw();
 
 function createLoopControls() {
-    const initialSliderValue = 2;
-    let timePerFrame = 1000 / initialSliderValue;
+  const initialSliderValue = 2;
+  let timePerFrame = 1000 / initialSliderValue;
 
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.title = 'auto step';
-    let loopInterval: NodeJS.Timeout;
-    checkbox.oninput = () => {
-        if (!checkbox.checked) return;
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.title = "auto step";
+  let loopInterval: NodeJS.Timeout;
+  checkbox.oninput = () => {
+    if (!checkbox.checked) return;
 
-        step();
-        loopInterval = setInterval(() => {
-            if (!checkbox.checked) {
-                clearInterval(loopInterval);
-                return;
-            }
-            step();
-        }, timePerFrame);
-    }
-
-    const slider = document.createElement('input');
-    slider.type = 'range';
-    slider.min = '1';
-    slider.max = '100'
-    slider.step = '1';
-    slider.value = initialSliderValue.toString();
-    slider.title = `steps per second- ${slider.value}`;
-
-    slider.oninput = () => {
-        timePerFrame = 1000 / Number(slider.value);
-        slider.title = `steps per second- ${slider.value}`;
+    step();
+    loopInterval = setInterval(() => {
+      if (!checkbox.checked) {
         clearInterval(loopInterval);
-        loopInterval = setInterval(() => {
-            if (!checkbox.checked) {
-                clearInterval(loopInterval);
-                return;
-            }
-            step();
-        }, timePerFrame);
+        return;
+      }
+      step();
+    }, timePerFrame);
+  };
 
-    }
-    return { slider, checkbox }
+  const slider = document.createElement("input");
+  slider.type = "range";
+  slider.min = "1";
+  slider.max = "100";
+  slider.step = "1";
+  slider.value = initialSliderValue.toString();
+  slider.title = `steps per second- ${slider.value}`;
+
+  slider.oninput = () => {
+    timePerFrame = 1000 / Number(slider.value);
+    slider.title = `steps per second- ${slider.value}`;
+    clearInterval(loopInterval);
+    loopInterval = setInterval(() => {
+      if (!checkbox.checked) {
+        clearInterval(loopInterval);
+        return;
+      }
+      step();
+    }, timePerFrame);
+  };
+  return { slider, checkbox };
 }
 
 function createStepButton() {
-    const button = document.createElement("button");
-    button.innerText = "Step"
-    button.onclick = step;
-    return button;
+  const button = document.createElement("button");
+  button.innerText = "Step";
+  button.onclick = step;
+  return button;
 }
 
 function setupControls() {
-    const stepButton = createStepButton();
-    const loopControls = createLoopControls();
+  const stepButton = createStepButton();
+  const loopControls = createLoopControls();
 
-    const loopContainer = document.createElement('div');
-    loopContainer.appendChild(loopControls.checkbox);
-    loopContainer.appendChild(loopControls.slider);
+  const loopContainer = document.createElement("div");
+  loopContainer.appendChild(loopControls.checkbox);
+  loopContainer.appendChild(loopControls.slider);
 
-    const controlsContainer = document.createElement('div');
-    controlsContainer.classList.add('controls')
-    controlsContainer.appendChild(stepButton);
-    controlsContainer.appendChild(loopContainer);
-    document.body.appendChild(controlsContainer);
+  const controlsContainer = document.createElement("div");
+  controlsContainer.classList.add("controls");
+  controlsContainer.appendChild(stepButton);
+  controlsContainer.appendChild(loopContainer);
+  document.body.appendChild(controlsContainer);
 }
