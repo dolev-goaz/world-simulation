db56183211f853892811130c575e9d1801a9b197
dolev goaz <www.dolev@gmail.com>
2023-12-28 17:04:12 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import path from "path";
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
     },
-}
+    base: './'
+});
